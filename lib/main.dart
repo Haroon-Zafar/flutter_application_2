f32e136f31ddd0f58f2d70df305f58714d09bb64
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-07-12 00:50:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Column(
+          children: [
+            // Addind Text Field
+            Container(
+              child: Container(
+                width: 200,
+                child: TextField(),
+              ),
+            ),
+          ],
+        ),
+        //   child: Center(
+        //     child: Column(
+        //       children: [
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         ),
+        //         Container(
+        //           height: 100,
+        //           width: 100,
+        //           color: Colors.purple,
+        //         ),
+        //         SizedBox(
+        //           height: 30,
+        //         )
+        //       ],
+        // ),
+        // ),
+      ),
+    );
+  }
+}

fbd15c1aab73d00b82219f4e184f84de0fda0735
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-07-12 01:54:39 +0500

    App Bar Title
    "Login Page"

--- lib/main.dart
+++ lib/main.dart
@@ -10,100 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Login Page"),
+          title: Text('Flutter Demo'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              SizedBox(
-                height: 250,
-              ),
-              // Addind Text Field
-              Container(
-                child: Container(
-                  width: 200,
-                  child: TextField(),
-                ),
-              ),
-              Container(
-                child: Container(
-                  width: 200,
-                  child: TextField(),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("Login")),
-            ],
-          ),
-        ),
-        //   child: Center(
-        //     child: Column(
-        //       children: [
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         ),
-        //         Container(
-        //           height: 100,
-        //           width: 100,
-        //           color: Colors.purple,
-        //         ),
-        //         SizedBox(
-        //           height: 30,
-        //         )
-        //       ],
-        // ),
-        // ),
       ),
     );
   }

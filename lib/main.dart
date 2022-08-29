7305fea0d522a55892c7f1f4e98a17acd8db064b
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-08-29 22:52:20 +0500

    Container
    Container widget takes only one child

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          height: 100,
-          width: 100,
+          height: 200,
+          width: 200,
           color: Colors.pink,
-          child: Text("Hello World"),
+          child: Text("Container"),
         ),
       ),
     );

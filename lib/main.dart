6f5ee094afb3dc3686076334efc8e021a58ca69d
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-08-29 22:58:35 +0500

    Children widgets inside Column() widget
    In Column() or Row() widgets we can take many children widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          height: 200,
-          width: 200,
-          color: Colors.pink,
-          child: Text("Container"),
+        body: Column(
+          children: [
+            Text("1st Column"),
+            Text("2nd Column"),
+            Text("3rd Column"),
+            Text("4th Column"),
+            Text("5th Column"),
+            Text("6th Column"),
+          ],
         ),
       ),
     );

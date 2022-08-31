93744a29c5f688fdd0fe9fb18d0f17b1584117fe
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-08-31 16:37:05 +0500

    Create a Home class

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Login Page")),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                width: 200,
-                child: TextField(),
-              ),
-              Container(
-                width: 200,
-                child: TextField(),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("Login"))
-            ],
-          ),
-        ),
-      ),
-    );
+    return Container();
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

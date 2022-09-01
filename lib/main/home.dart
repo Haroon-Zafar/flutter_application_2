016388460ca13fc092dd2dc39f93aabfc67a0012
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-02 00:38:15 +0500

    Asset Image is adde
    Asset Image is added and once you add a folder or something in it ( i.e asset folder) you must update it in pungsec.yaml file in folder structure because of dependencies in the project.

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -14,8 +14,7 @@ class _HomeState extends State<Home> {
       body: Center(
         child: CircleAvatar(
           radius: 100,
-          backgroundImage: NetworkImage(
-              'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&w=1000&q=80'),
+          backgroundImage: AssetImage('assets/bg.png'),
         ),
       ),
     );

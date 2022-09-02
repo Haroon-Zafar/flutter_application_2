df65bd7c447d99679073df40a693fd03a184212c
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-02 21:46:58 +0500

    Stack( )
    When we want pictures upon pictures.

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -11,11 +11,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircleAvatar(
-          radius: 100,
-          backgroundImage: AssetImage('assets/bg.png'),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            height: 500,
+            width: 500,
+          ),
+          Container(
+            color: Colors.blue,
+            height: 400,
+            width: 400,
+          ),
+          Container(
+            color: Colors.pink,
+            height: 300,
+            width: 300,
+          ),
+          Container(
+            color: Colors.purple,
+            height: 200,
+            width: 200,
+          ),
+          Container(
+            color: Colors.yellow,
+            height: 100,
+            width: 100,
+          ),
+        ],
       ),
     );
   }

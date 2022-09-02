734e78006fff67f6f85d3724e6b0816ed24f48ae
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 03:59:33 +0500

    How to Make a Widget ?
    copy piece of code you want to reuse again and again

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -15,29 +15,31 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/bg.png'),
-              radius: 20,
-            ),
-            title: Text("Haroon"),
-            subtitle: Text("hari_zafar"),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  "4:25 PM",
-                ),
-                CircleAvatar(
-                  backgroundColor: Colors.green,
-                  radius: 10,
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: [],
       ),
     );
   }
+}
+
+Widget ListTile() {
+  return ListTile(
+    leading: CircleAvatar(
+      backgroundImage: AssetImage('assets/bg.png'),
+      radius: 20,
+    ),
+    title: Text("Haroon"),
+    subtitle: Text("hari_zafar"),
+    trailing: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          "4:25 PM",
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.green,
+          radius: 10,
+        ),
+      ],
+    ),
+  );
 }

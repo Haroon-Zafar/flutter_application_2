1f5d53876dae1c86a4cebce7141a959f019dd445
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 00:22:02 +0500

    Responsive Height, Width

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -11,50 +11,8 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              color: Colors.red,
-              height: 100,
-              width: 100,
-            ),
-          ),
-          Positioned(
-            left: 10,
-            top: 10,
-            child: Container(
-              color: Colors.purple,
-              height: 100,
-              width: 100,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              color: Colors.pink,
-              height: 100,
-              width: 100,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              color: Colors.purple,
-              height: 100,
-              width: 100,
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              color: Colors.yellow,
-              height: 100,
-              width: 100,
-            ),
-          )
-        ],
+      body: Container(
+        color: Colors.purple,
       ),
     );
   }

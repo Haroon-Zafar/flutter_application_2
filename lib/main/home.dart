529911fd9a27925f88655143c009b91695125507
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 00:33:21 +0500

    Accessing Other Page using Button

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -12,11 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          color: Colors.purple,
-          height: MediaQuery.of(context).size.height * 0.5,
-          width: MediaQuery.of(context).size.width * 0.5,
-        ),
+        child:
       ),
     );
   }

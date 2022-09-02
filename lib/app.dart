335aa82892d51a7dd2ec72eec79992e70ce3fd82
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 00:39:31 +0500

    Create app.dart
    Elevated Button will send us to app.dart File.

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({ Key? key }) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("App Page", style: TextStyle(fontSize: 30),),
+      
+    );
+  }
+}}

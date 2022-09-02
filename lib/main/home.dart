0d26af10e940898f7efa7549f5a5b07b5e215bf0
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 00:31:12 +0500

    wrapping with Center

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -11,10 +11,12 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.purple,
-        height: MediaQuery.of(context).size.height * 0.5,
-        width: MediaQuery.of(context).size.width * 0.5,
+      body: Center(
+        child: Container(
+          color: Colors.purple,
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width * 0.5,
+        ),
       ),
     );
   }

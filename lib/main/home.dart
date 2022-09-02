21b2fe8d383d819cd1ecedfcdf7ae3315f8ca217
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 03:37:42 +0500

    ListTile() widget

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -14,14 +14,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => App()));
-          },
-          child: Text("Button"),
-        ),
+      body: Column(
+        children: [
+          ListTile(),
+        ],
       ),
     );
   }

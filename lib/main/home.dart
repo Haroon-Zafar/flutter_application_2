5884526d50655fba5574117277ae8398c32ca68f
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 22:27:49 +0500

    Creating a Widget which returns Dynamic Value
    Before this we have created widgets which were all returning the same values but now we are trying to return dynamic values for Widget

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -20,32 +20,7 @@ class _HomeState extends State<Home> {
             color: Colors.yellow,
             height: 100,
             child: Row(children: [
-              Container(
-                color: Colors.blue,
-                height: 95,
-                width: 95,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("ABC"),
-                    Row(
-                      children: [
-                        Text("ABC"),
-                        Text("ABC"),
-                        Text("ABC"),
-                      ],
-                    ),
-                    Text("ABC"),
-                    Icon(Icons.notifications),
-                    Text("ABC"),
-                  ],
-                ),
-              ),
-            ]),
-          ),
+              
         ],
       ),
     );

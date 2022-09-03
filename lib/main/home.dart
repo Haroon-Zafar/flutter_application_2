ed973ec9ba88a2dd02eb9bad2aae51a6fe48cd5e
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-03 22:57:48 +0500

    ListView.Builder()

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -14,36 +14,37 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          abc("Haroon"),
-        ],
+      body: ListView.builder(
+        itemCount: ,
+        itemBuilder: itemBuilder),
       ),
     );
   }
 }
 
-Widget abc(var name) {
-  return Container(
-    color: Colors.yellow,
-    height: 100,
-    child: Row(
-      children: [
-        Container(
-          color: Colors.blue,
-          height: 95,
-          width: 95,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(name),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
-}
+
+
+// Widget abc(var name) {
+//   return Container(
+//     color: Colors.yellow,
+//     height: 100,
+//     child: Row(
+//       children: [
+//         Container(
+//           color: Colors.blue,
+//           height: 95,
+//           width: 95,
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.only(left: 10.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Text(name),
+//             ],
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }

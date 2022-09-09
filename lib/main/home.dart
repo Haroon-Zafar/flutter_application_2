26a9bce067def4a2e8d2291ee76e0d39ab40a3c5
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-09 23:37:04 +0500

    children: List.generate()
    without square brackets

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -12,16 +12,18 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var lst = [
-    "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2Fyc3xlbnwwfHwwfHw%3D&w=1000&q=80",
-    "https://images.unsplash.com/photo-1601362840469-51e4d8d58785?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Ym13JTIwY2FyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    "Ali",
+    "Ahmed",
+    "Huzaifa",
+    "Bilal",
   ];
-  var age = ["18", "19", "20", "21"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView.count(
         crossAxisCount: 2,
-        children: [],
+        children: List.generate(lst.length, (index) {}),
       ),
     );
   }

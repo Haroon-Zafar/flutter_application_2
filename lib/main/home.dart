31fb1609cdcbd53c55f0b0b28e25d21fb0d6dc68
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-09 23:47:08 +0500

    crossAxisSpacing:
    It gives space between both widgets in row wise

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: GridView.count(
           crossAxisCount: 2,
+          crossAxisSpacing: 50,
           children: List.generate(lst.length, (index) {
             return Container(
               height: 200,
